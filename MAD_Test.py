7266a03e65f1a4abbff498f0f9e9ebf676be381d
ThomIves <thom.ives@gmail.com>
2019-09-18 06:47:33 -0600

    pep8'd the code and updated blog post

--- MAD_Test.py
+++ MAD_Test.py
@@ -2,12 +2,14 @@ from sklearn.linear_model import LinearRegression
 from ToolKit import MAD
 
 ###############################################################################
-### Model info section
+# ## Model info section
 
-model = LinearRegression(normalize=True,copy_X=False)
+model = LinearRegression(normalize=True, copy_X=False)
 
-Notes = """Logging the model with TWO non-default parameter and logging all aspects.
-Removed the py_version, pip_requirements and imports logging this time."""
+Notes = """Logging the model with TWO non-default parameters
+    and logging all aspects. Removed the py_version, pip_requirements and
+    imports logging this time."""
 
-MAD(model, extra_notes=Notes) #, py_version=False, pip_requirements=False, imports=False) 
-#, capture_notes=False)
+MAD(model, extra_notes=Notes) 
+# , py_version=False, pip_requirements=False, imports=False)
+# , capture_notes=False)
--- ToolKit.py
+++ ToolKit.py
@@ -7,45 +7,50 @@ from datetime import datetime
 
 
 class MAD:
-    """ "Model and Dependencies" - capture machine learning model 
+    """ "Model and Dependencies" - capture machine learning model
             settings and dependencies:
 
-        0) Create a model_logs directory if it does not exist, and 
+        0) Create a model_logs directory if it does not exist, and
             save log files with a time stamp (or not) to that directory.
-        1) Document the machine learning model name and it's non-default parameters.
+        1) Document the machine learning model name
+            and it's non-default parameters.
         2) Document the version of python in use.
         3) Document the pip requirements.
         4) Document the necessary imports.
-        5) Document any other important notes not covered above, 
+        5) Document any other important notes not covered above,
             such as how missing values were filled, etc.
     """
 
     def __init__(self, mod, 
-        file_name='model_data.txt', extra_notes='', add_time_stamp=True, **WTL):
+        file_name='model_data.txt', extra_notes='',
+        add_time_stamp=True, **WTL):
         """Perform setups for auto documentation.
-        
-        The first line captures the filename of the script instantiating this class.
-        The second line captures the locals from the same script in the first step.
+
+        The first line captures the filename of the script
+            instantiating this class.
+        The second line captures the locals from the same script
+            in the first step.
         The if block adds a time stamp to the default, or provided, file_name.
         The next if block adds a model logs directory IF it does not yet exist.
-        The file open to write context manager then creates a time stamped file,
-            if add_time_step is true, and adds a documentation section captured 
-            from each method.
+        The file open to write context manager then creates a
+            time stamped file, if add_time_step is true, and adds
+            a documentation section captured from each method.
 
         Arguments:
             mod {class instance} --  instance of machine learning class
-        
+
         Keyword Arguments:
-            file_name {str} -- the filename used for the log file 
+            file_name {str} -- the filename used for the log file
                 (default: {'model_data.txt'})
-            extra_notes {str} -- optional notes providing mode detail 
+            extra_notes {str} -- optional notes providing mode detail
                 (default: {''})
-            add_time_stamp {bool} -- boolean to add timp stamp to model file or not 
-                (default: {True})
-            WTL {kwargs} -- What To Log (WTL) are key word arguments for what to log. 
-                The default is to log everything. Pass any py_version=False, 
-                pip_requirements=False, imports=False, and/or capture_notes=False 
-                to NOT log one or more of these.
+            add_time_stamp {bool} -- bool to add timp stamp to model file
+                or not (default: {True})
+            WTL {kwargs} -- What To Log (WTL) are key word arguments
+                for what to log. The default is to log everything.
+                Pass any py_version=False, pip_requirements=False,
+                imports=False, and/or capture_notes=False to NOT log one
+                or more of these.
         """
         # Section A.1
         self.calling_file = __main__.__file__
@@ -62,14 +67,17 @@ class MAD:
         # Section A.3
         with open('./model_logs/' + file_name, 'w') as self.out_file:
             self.out_file.write(self.get_model_info(mod))
-            if (('py_version' not in WTL) or (WTL['py_version'] == True)): 
+            if (('py_version' not in WTL) or (WTL['py_version'] is True)):
                 self.out_file.write(self.get_python_version())
-            if (('pip_requirements' not in WTL) or (WTL['pip_requirements'] == True)): 
+            if (('pip_requirements' not in WTL) or
+                    (WTL['pip_requirements'] is True)):
                 self.out_file.write(self.get_pip_requirements())
-            if (('imports' not in WTL) or (WTL['imports'] == True)): 
+            if (('imports' not in WTL) or (WTL['imports'] is True)):
                 self.out_file.write(self.get_necessary_imports())
-            if (('capture_notes' not in WTL) or (WTL['capture_notes'] == True)): 
-                self.out_file.write('\n' + '# Extra Notes:\n' + extra_notes)
+            if (('capture_notes' not in WTL) or
+                    (WTL['capture_notes'] is True)):
+                self.out_file.write(
+                    '\n' + '# Extra Notes:\n' + extra_notes)
 
     def _get_model_string(self, model):
         """Internal function that returns a string of the full model call
@@ -79,10 +87,10 @@ class MAD:
         Returns:
             a string of the full model call
         """
-        model_string   = str(model).replace('\n', '').replace(' ', '')
+        model_string = str(model).replace('\n', '').replace(' ', '')
 
         return model_string
-        
+
     def _get_model_name(self, model_string):
         """Internal function to get the model name
         Arguments:
@@ -97,45 +105,48 @@ class MAD:
     def _get_model_params_array(self, model_string):
         """docstring here
         Arguments:
-            :param model_string: the string of the instance call of the model class
+            :param model_string: the string of the instance call
+                of the model class
         Returns:
             {list} a list of the model instance parameters
         """
         model_name = self._get_model_name(model_string)
         model_params_array = model_string.replace(
-            model_name,'').replace('(','').replace(')','').split(',')
+            model_name, '').replace('(', '').replace(')', '').split(',')
 
         return model_params_array
 
     def get_model_info(self, mod):
-        """Simple class method to return a formatted string of model information.
+        """Simple method to return a formatted string of model information.
         Arguments:
-            :param mod: {class instance} -- argument passed from __init__ method
+            :param mod: {class instance} -- argument from __init__ method
         Returns:
             {str} the model with non default parameters in use
         """
         # Section B.1: get the model string and parameters
         model_string = self._get_model_string(mod)
         if 'Pipeline' in model_string:
-            model_string = model_string.replace(',',',\n\t\t')
+            model_string = model_string.replace(',', ',\n\t\t')
             return '# Model and parameters:\n\t' + model_string + '\n'
 
         model_name   = self._get_model_name(model_string)
         model_params = self._get_model_params_array(model_string)
-        
-        # Section B.2a: get the params used in the default instance of the model
-        default_imports_array  = [
+
+        # Section B.2a: get the params used in the default instance
+        #     of the model
+        default_imports_array = [
             x for x in self._get_imports_array() if model_name in x]
         default_imports_string = "\n".join(default_imports_array)
-        default_exec_command   = default_imports_string + ";" + "mod_default=" \
+        default_exec_command = default_imports_string + ";" + "mod_default=" \
             + model_name + "()"
         exec(default_exec_command, globals(), locals())
         # Section B.2b: get the default model string and parameters
         default_model_string = self._get_model_string(locals()['mod_default'])
-        default_model_params = self._get_model_params_array(default_model_string)
+        default_model_params = self._get_model_params_array(
+            default_model_string)
 
-        # Section B.3: get the list of non default parameters and create a model string 
-        #     with non default parameters
+        # Section B.3: get the list of non default parameters and create
+        #     a model string with non default parameters
         non_default_model_params = [
             x for x in model_params if x not in default_model_params]
         if len(non_default_model_params) == 0:
@@ -147,24 +158,24 @@ class MAD:
         return '# Model and parameters:\n\t' + log_model_string + '\n'
 
     def get_python_version(self):
-        """Simple class method to return a string of python version information.
+        """Simple method to return a string of python version information.
         """
         return '\n# python version:\n' + str(sys.version.split('\n')[0] + '\n')
 
     def get_pip_requirements(self):
         """Class method to capture pip requirements for the script.
-        
-        The first code block creates a string out of the local items. 
-        The second block does a pip version flexible import of freeze to capture 
-            modules loaded by pip.
-        The third block obtains all pip installs that appear in local items also
-            and puts them in a data frame.
+
+        The first code block creates a string out of the local items.
+        The second block does a pip version flexible import of freeze
+            to capture modules loaded by pip.
+        The third block obtains all pip installs that appear
+            in local items also and puts them in a data frame.
         The fourth block formats a requirements.txt style string of required
             pip modules needed by the script instantiating this class.
-        
+
         Returns:
-            {str} -- a formatted list of modules needing pip installation for the 
-                        model to work.
+            {str} -- a formatted list of modules needing pip installation
+                     for the model to work.
         """
         # Section C.1
         local_modules_string = str(self.locals.items())
@@ -201,19 +212,19 @@ class MAD:
                 ('#' != line[0]) and ('MAD' not in line))]
 
         return imprt_lines
-        
+
     def get_necessary_imports(self):
-        """Returns a formatted string of imports needed by the script 
+        """Returns a formatted string of imports needed by the script
             instantiating this class.
 
         The first code block reads the file instantiating this class, and
-            filters a list of the file lines to find those containing 
+            filters a list of the file lines to find those containing
             import statements.
-        The second code block prepares a formatted string for return of 
+        The second code block prepares a formatted string for return of
             the imports captured in the first block.
 
         Returns:
-            {str} -- a formatted list of imports from the script of 
+            {str} -- a formatted list of imports from the script of
                         code block one.
         """
         # Section D.1
